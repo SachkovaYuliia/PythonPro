dd95a6dddbacae1d959417c6b36d6bc92ce85313
sachkova_yuliia <sachkova_yuliia@gmail.com>
2024-09-19 20:59:45 +0200

    updated file

--- PythonPro.py
+++ PythonPro.py
@@ -1,1 +1,8 @@
 print("It is first homework!:)")
+
+squares = ['red', 'yellow', 'green', 'purple', 'blue']
+
+for i in range(0, 5):
+    print("Before square ", i, 'is',  squares[i])
+    squares[i] = 'white'
+    print("After square ", i, 'is',  squares[i])
